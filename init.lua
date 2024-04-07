5f0e8bf461741e0a7ee9def162ea96157effd037
minhhoccode111 <minhhoccode111@gmail.com>
2024-04-07 16:52:28 +0700

    Fix: Gitsigns description

--- init.lua
+++ init.lua
@@ -322,7 +322,7 @@ require('lazy').setup({
           else
             gitsigns.nav_hunk 'next'
           end
-        end, { desc = 'Gitsigns [H]unk Next[j]' })
+        end, { desc = 'Next[j] Hunk' })
 
         map('n', '<leader>hk', function()
           if vim.wo.diff then
@@ -330,30 +330,30 @@ require('lazy').setup({
           else
             gitsigns.nav_hunk 'prev'
           end
-        end, { desc = 'Gitsigns [H]unk Prev[k]' })
+        end, { desc = 'Prev[k] Hunk' })
 
         -- setup keymaps, mine
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Gitsigns [H]unk [S]tage' })
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Gitsigns [H]unk [R]eset' })
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = '[S]tage Hunk' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = '[R]eset Hunk' })
         map('v', '<leader>hs', function()
           gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'Gitsigns [H]unk [S]tage' })
+        end, { desc = '[S]tage Hunk' })
         map('v', '<leader>hr', function()
           gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'Gitsigns [H]unk [R]eset Line' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Gitsigns [H]unk [S]tage' })
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Gitsigns [H]unk [U]ndo Stage' })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Gitsigns [H]unk [R]eset' })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Gitsigns [H]unk [P]review' })
+        end, { desc = '[R]eset Hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = '[S]tage Hunk Buffer' })
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = '[U]ndo Stage Hunk' })
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = '[R]eset Hunk Buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = '[P]review Hunk' })
         map('n', '<leader>hb', function()
           gitsigns.blame_line { full = true }
-        end, { desc = 'Gitsigns [H]unk [B]lame Line' })
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Gitsigns [T]oggle Blame Line' })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Gitsigns [H]unk [d]iff' })
+        end, { desc = '[B]lame Line' })
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Gitsigns [B]lame Line' })
+        map('n', '<leader>hd', gitsigns.diffthis, { desc = '[d]iff this' })
         map('n', '<leader>hD', function()
           gitsigns.diffthis '~'
-        end, { desc = 'Gitsigns [H]unk [D]iff ~' })
-        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = 'Gitsigns [T]oggle Line Deleted' })
+        end, { desc = '[D]iff ~' })
+        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = 'Gitsigns [D]eleted Line' })
       end,
     },
   },
