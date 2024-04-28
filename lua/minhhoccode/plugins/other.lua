bf92b415a2751b932ceeba184c439436738189f0
minhhoccode111 <minhhoccode111@gmail.com>
2024-04-28 23:04:27 +0700

    Add: tailwind colorier

--- lua/minhhoccode/plugins/other.lua
+++ lua/minhhoccode/plugins/other.lua
@@ -2,7 +2,7 @@
 
 return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -10,6 +10,15 @@ return {
   {
     -- { 'windwp/nvim-ts-autotag' },
     -- opts = {},
+  },
+
+  {
+    'NvChad/nvim-colorizer.lua',
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
   },
 }
 
