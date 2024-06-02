65904b2903f50cbbbd4c26519ee3929efa2b551c
minhhoccode111 <minhhoccode111@gmail.com>
2024-06-02 11:21:32 +0700

    fix extra space in mini.surround

--- lua/minhhoccode/plugins/mini.lua
+++ lua/minhhoccode/plugins/mini.lua
@@ -10,16 +10,26 @@ return {
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [']quote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      require('mini.ai').setup { n_lines = 100 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup(
-        --
-      )
+      require('mini.surround').setup {
+        n_lines = 100,
+
+        -- default
+        -- ['('] = { output = { left = '( ', right = ' )' } },
+        -- remove extra space
+        custom_surroundings = {
+          ['('] = { output = { left = '(', right = ')' } },
+          ['{'] = { output = { left = '{', right = '}' } },
+          ['['] = { output = { left = '[', right = ']' } },
+          ['<'] = { output = { left = '<', right = '>' } },
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
